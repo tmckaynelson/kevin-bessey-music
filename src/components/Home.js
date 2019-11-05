a22b4eaeef4ad0f0efc4f522d65a5b9020c8317a
McKay Nelson <tmckaynelson@gmail.com>
2019-11-05 12:25:32 -0700

    mobile responsive site complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,7 @@ import Tour from './Tour'
 export default function Home() {
     return (
         <div>
-                    <div className="banner-img"> 
-                {/* <img src={ banner } alt="The Patient banner" /> */}
-            </div>
+            <div className="banner-img"/>
             <div className="iframe">
                 {<iframe src="https://open.spotify.com/embed/track/0N1DnRp0d5PdOqFvAxLgTB" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>}
             </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+
 
 import './Nav.css'
 
 export default function Nav(props) {
+
+    const [ slide, setSlide ] = useState(true)
+
+    const socialIcons = (
+        <>
+            <a href="https://www.facebook.com/knbessey/" target="_blank"><li><i class="fab fa-facebook"></i></li></a>
+            <a href="https://www.instagram.com/knbessey/" target="_blank"><li><i class="fab fa-instagram"></i></li></a>
+            <a href="https://open.spotify.com/artist/53h4243mR88lDJIjU5JsOb" target="_blank"><li><i class="fab fa-spotify"></i></li></a>
+            <a href="https://itunes.apple.com/ca/artist/kevin-bessey/1349463595" target="_blank"><li><i class="fab fa-apple"></i></li></a>
+            <a href="https://www.youtube.com/channel/UCn-uhlQmEajH8TRKcphdRhA" target="_blank"><li><i class="fab fa-youtube"></i></li></a>
+            <a href="https://kevinbessey.bandcamp.com" target="_blank"><li><i class="fab fa-bandcamp"></i></li></a>
+        </>
+    )
+
+    const links = (
+        <>
+            {/* only display links if logged in as admin */}
+            {
+                props.user ?
+                <>
+                    <Link to="/admin" className="link"><li>Admin Dashboard</li></Link>
+                    <Link to="/" className="link"><li>Home</li></Link>
+                </>
+                :
+                null
+            }
+            <a href="https://kevinbessey.bandcamp.com/merch" target="_blank" className="link"><li>Merch</li></a>
+        </>
+    )
+
+    const toggleMenu = () => {
+        setSlide(!slide)
+    }
+
     return (
         <nav className="navbar">
             <div className="nav-container">
                 <h1>Kevin Bessey</h1>
                 <ul className="nav-menu">
-
-                    {/* only display links if logged in as admin */}
-                    {
-                        props.user ?
-                        <>
-                            <Link to="/admin" className="link"><li>Admin Dashboard</li></Link>
-                            <Link to="/" className="link"><li>Home</li></Link>
-                        </>
-                        :
-                        null
-                    }
-                    <a href="https://kevinbessey.bandcamp.com/merch" target="_blank" className="link"><li>Merch</li></a>
-                    <a href="https://www.facebook.com/knbessey/" target="_blank"><li><i class="fab fa-facebook"></i></li></a>
-                    <a href="https://www.instagram.com/knbessey/" target="_blank"><li><i class="fab fa-instagram"></i></li></a>
-                    <a href="https://open.spotify.com/artist/53h4243mR88lDJIjU5JsOb" target="_blank"><li><i class="fab fa-spotify"></i></li></a>
-                    <a href="https://itunes.apple.com/ca/artist/kevin-bessey/1349463595" target="_blank"><li><i class="fab fa-apple"></i></li></a>
-                    <a href="https://www.youtube.com/channel/UCn-uhlQmEajH8TRKcphdRhA" target="_blank"><li><i class="fab fa-youtube"></i></li></a>
-                    <a href="https://kevinbessey.bandcamp.com" target="_blank"><li><i class="fab fa-bandcamp"></i></li></a>
+                    { links }                    
+                    { socialIcons }
+                </ul>
+                <div className="mobile-menu" onClick={ toggleMenu }>
+                    { slide ? <i class="fas fa-times"></i> : <i class="fas fa-bars"></i> }
+                </div>
+            </div>
+            <div className={ slide ? 'slideout slide' : 'slideout'}>
+                <ul className="nav-links">
+                    { links }
+                </ul>
+                <ul className="social-links">
+                    { socialIcons }
                 </ul>
             </div>
         </nav>
